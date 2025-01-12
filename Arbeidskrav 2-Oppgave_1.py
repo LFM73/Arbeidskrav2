f16dd78cebdbcc7a380b1798a6d1074243b63427
LFM73 <126172359+LFM73@users.noreply.github.com>
2025-01-12 20:33:44 +0100

    Add files via upload

---
+++ Arbeidskrav 2-Oppgave_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <b> Oppgave 1 <b/>
+
+# In[3]:
+
+
+alder = int(input('Hvilket år er du født? ') )
+
+print("I år, 2025, blir du", 2025 - alder, "år")
+
+
+# In[ ]:
+
+
+
+
